5902168461ef59d374addb5684ba83f669c78b1c
dangvpham <106801128+dangvpham@users.noreply.github.com>
2023-03-06 17:23:16 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
       name = "xcp-ng"
     }
   }
-  required_version = ">= 1.1.0"
+ # required_version = ">= 1.1.0"
   required_providers {
     random = {
       source  = "hashicorp/random"
